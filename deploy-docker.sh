23fc44a811394e0856e0bd112b20b0667070bf2c
Russel Waters <russel@nano.org>
2019-06-26 21:41:00 -0400

    rename

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -6,7 +6,7 @@ if [ -n "$DOCKER_PASSWORD" ]; then
 
     # We push this just so it can be a cache next time
     if [ "$TRAVIS_BRANCH" = "master" ]; then
-            ci_image_name="argakiig/nano-build-env-${COMPILER}"
+            ci_image_name="argakiig/nano-build-env-$TRAVIS_JOB_NAME"
             build-docker-image.sh docker/ci/Dockerfile-${compiler} ${ci_image_name};
             docker push "$ci_image_name"
     fi
