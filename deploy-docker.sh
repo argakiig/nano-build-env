c0c74b8caedddb6fde01639d9a68f38aaf62899f
Russel Waters <russel@nano.org>
2019-06-26 22:20:49 -0400

    executable scripts are good

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -7,7 +7,7 @@ if [ -n "$DOCKER_PASSWORD" ]; then
     # We push this just so it can be a cache next time
     if [ "$TRAVIS_BRANCH" = "master" ]; then
             ci_image_name="argakiig/nano-build-env-$TRAVIS_JOB_NAME"
-            build-docker-image.sh docker/ci/Dockerfile-${compiler} ${ci_image_name};
+            ./build-docker-image.sh Dockerfile-${TRAVIS_JOB_NAME} ${ci_image_name};
             docker push "$ci_image_name"
     fi
 
